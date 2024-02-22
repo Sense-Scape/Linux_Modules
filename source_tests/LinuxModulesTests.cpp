41d2cd1f384789c2b1dc0126965cfc2157ed1056
Grabt234 <gnorrie12@gmail.com>
2024-02-22 12:56:23 +0200

    Feat: Adding a broken test

--- source_tests/LinuxModulesTests.cpp
+++ source_tests/LinuxModulesTests.cpp
@@ -1,12 +1,5 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 
-#include "doctest.h"
-
-TEST_CASE("Generic Test")
-{
-    SUBCASE("Checking default constructor") {
-        CHECK(1==1);
-    }
-
-}
+// L
+#include "LinuxMultiClientTCPTxModuleTests.h"
